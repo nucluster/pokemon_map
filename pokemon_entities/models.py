b2d199fd7e487b9fa4458bad2339a9b4cf06c337
nucluster <nucluster@gmail.com>
2021-05-06 00:14:17 +0500

    step 7

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,3 +7,12 @@ class Pokemon(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    lat = models.FloatField(null=True, blank=True)
+    lon = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.pokemon.title + ' Entity'
