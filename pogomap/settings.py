44654dc84497eec0072b8b8ea2bc1be945656c9e
nucluster <nucluster@gmail.com>
2021-05-05 19:56:30 +0500

    add database settings

--- pogomap/settings.py
+++ pogomap/settings.py
@@ -80,11 +80,16 @@ WSGI_APPLICATION = 'pogomap.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": env.dj_db_url("DATABASE_URL",
+                            f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}")
 }
 
 
