d0fe2104ba06ed110919684bc98b0500485fb13b
nucluster <nucluster@gmail.com>
2021-05-07 17:38:39 +0500

    modif models.py from pokemons.json

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
-    
+
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200, blank=True)
+    title_ru = models.CharField(max_length=200, blank=True)
+    title_en = models.CharField(max_length=200, blank=True, null=True)
+    title_jp = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    img_url = models.URLField(blank=True, null=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        return self.title_en
 
 
 class PokemonEntity(models.Model):
@@ -16,13 +20,13 @@ class PokemonEntity(models.Model):
     appeared_at = models.DateTimeField(null=True, blank=True)
     disappeared_at = models.DateTimeField(null=True, blank=True)
     level = models.IntegerField(null=True, blank=True, verbose_name='Уровень')
-    health = models.IntegerField(null=True, blank=True, verbose_name='Здоровье')
+    health = models.IntegerField(null=True, blank=True,
+                                 verbose_name='Здоровье')
     defence = models.IntegerField(null=True, blank=True, verbose_name='Защита')
     stamina = models.IntegerField(null=True, blank=True,
-                                    verbose_name='Выносливость')
+                                  verbose_name='Выносливость')
     strength = models.IntegerField(null=True, blank=True,
-                                    verbose_name='Атака')
-
+                                   verbose_name='Атака')
 
     def __str__(self):
-        return self.pokemon.title + ' Entity'
+        return self.pokemon.title_en + ' Entity'
