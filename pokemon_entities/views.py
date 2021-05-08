55bdc97f280bf5ba0aa5d2fc8b640d07e4158a33
nucluster <nucluster@gmail.com>
2021-05-09 01:31:39 +0500

    ref

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -31,17 +31,8 @@ def show_all_pokemons(request):
                 pokemon.img_url,
             )
 
-    # pokemons_on_page = []
-    # for pokemon in pokemons:
-    #     pokemons_on_page.append({
-    #         'pokemon_id': pokemon.id,
-    #         'img_url': pokemon.img_url,
-    #         'title_ru': pokemon.title_ru,
-    #     })
-
     return render(request, 'mainpage.html', context={
-        'map': folium_map._repr_html_(),
-        'pokemons': pokemons,
+        'map': folium_map._repr_html_(), 'pokemons': pokemons
     })
 
 
